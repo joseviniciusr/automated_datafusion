e3e304a7ce7a34285cf09c8a8bce3f4d41400685
J V <163000212+joseviniciusr@users.noreply.github.com>
2025-03-18 09:16:05 -0300

    Update automated_datafusion.py

--- automated_datafusion.py
+++ automated_datafusion.py
@@ -3,9 +3,9 @@
  
 **Description**:
 
-Additionally to modeling (by PLS, RF or SVM), the individual modeling function independently extracts the Latent Variable Scores (-based on PLS transform).
+The data fusion functions are mainly powered by dictionaries where the keys represent all employed soucers (m). Then, all possible combinations ranging from 2 to m are modeled along with several performance metrics are extracted.
 
-The data fusion functions are mainly powered by dictionaries where the keys represent all employed sensors (m). Then, all possible combinations ranging from 2 to m are modeled along with several performance metrics are extracted.
+Additionally to modeling (implemented by PLS, RF or SVM), the individual modeling function independently extracts the Latent Variable Scores (-based on PLS transform).
 
 **Author**: Msc. Jose Vinicius Ribeiro
 
